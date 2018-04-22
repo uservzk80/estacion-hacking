adc7a107f47dae11c2f77eb7236cc5ff71b54a67
Israel Pérez <pisrael252@gmail.com>
2018-04-22 17:12:24 +0200

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,97 @@
+import telebot
+from telebot import types
+import time
+import os
+TOKEN = 'BOT TOKEN'
+
+ALLOWED_USERS = {"User":"User ID", "Anoter User":"Another ID"}
+
+knownUsers = []  # todo: save these in a file,
+userStep = {}  # so they won't reset every time the bot restarts
+
+commands = {  # command description used in the "help" command
+              'start': 'Get used to the bot',
+              'help': 'Gives you information about the available commands',
+              'exec': 'Execute any linux command'
+}
+
+# error handling if user isn't known yet
+# (obsolete once known users are saved to file, because all users
+#   had to use the /start command and are therefore known to the bot)
+def get_user_step(uid):
+    if uid in userStep:
+        return userStep[uid]
+    else:
+        knownUsers.append(uid)
+        userStep[uid] = 0
+        print ("New user detected, who hasn't used \"/start\" yet")
+        return 0
+
+
+# only used for console output now
+def listener(messages):
+    """
+    When new messages arrive TeleBot will call this function.
+    """
+    for m in messages:
+        if m.content_type == 'text':
+            # print the sent message to the console
+            print (str(m.chat.first_name) + " [" + str(m.chat.id) + "]: " + m.text)
+
+
+bot = telebot.TeleBot(TOKEN)
+bot.set_update_listener(listener)  # register listener
+
+
+# handle the "/start" command
+@bot.message_handler(commands=['start'])
+def command_start(m):
+    cid = m.chat.id
+    if cid not in knownUsers:  # if user hasn't used the "/start" command yet:
+        knownUsers.append(cid)  # save user id, so you could brodcast messages to all users of this bot later
+        userStep[cid] = 0  # save user id and his current "command level", so he can use the "/getImage" command
+        bot.send_message(cid, "Hello, stranger, let me scan you...")
+        bot.send_message(cid, "Scanning complete, I know you now")
+        command_help(m)  # show the new user the help page
+    else:
+        bot.send_message(cid, "I already know you, no need for me to scan you again!")
+
+
+# help page
+@bot.message_handler(commands=['help'])
+def command_help(m):
+    cid = m.chat.id
+
+    help_text = "The following commands are available: \n"
+    for key in commands:  # generate help text out of the commands dictionary defined at the top
+        help_text += "/" + key + ": "
+        help_text += commands[key] + "\n"
+    bot.send_message(cid, help_text)  # send the generated help page
+
+@bot.message_handler(commands=['exec'])
+def execute(m):
+
+    cid = m.chat.id
+
+    if str(cid) in ALLOWED_USERS.values():
+
+        text = m.text.replace("/exec", "")
+        command = os.popen(text).read()
+        bot.send_message(m.chat.id, ">>> %s" % (command))
+
+    else:
+        bot.send_message(m.chat.id, "NOT ALLOWED.")
+
+# filter on a specific message
+@bot.message_handler(func=lambda message: message.text == "hi")
+def command_text_hi(m):
+    bot.send_message(m.chat.id, "I love you too!")
+
+
+# default handler for every other text
+@bot.message_handler(func=lambda message: True, content_types=['text'])
+def command_default(m):
+    # this is the standard reply to a normal message
+    bot.send_message(m.chat.id, "I don't understand \"" + m.text + "\"\nMaybe try the help page at /help")
+
+bot.polling()
